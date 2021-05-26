ebdc65d3aeec892da17fe43e89e68175e0ea35b2
vivek1305 <vivekkumar.sit13@gmail.com>
2021-05-26 16:13:55 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version = "0.0.1",
+    description = "This is wine Q package",
+    author="vivek1305",
+    packages=find_packages(),
+    license="MIT"
+)
